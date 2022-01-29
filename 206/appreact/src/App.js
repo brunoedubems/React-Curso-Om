24dbebce5899f4f8921a492b0aa4ea31dec470b0
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-01-29 17:49:26 -0300

    Update App.js

--- 206/appreact/src/App.js
+++ 206/appreact/src/App.js
@@ -1,34 +1,63 @@
 import React from 'react';
 
+const luana = {
+  cliente: 'luana',
+  idade: 27,
+  compras: [{
+      nome: 'Notebook',
+      preco: 'R$ 2500'
+    },
+    {
+      nome: 'Geladeira',
+      preco: 'R$ 3000'
+    },
+    {
+      nome: 'Celular',
+      preco: 'R$ 1500'
+    },
+  ],
+  ativa: true,
+}
+const Mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [{
+      nome: 'Notebook',
+      preco: 'R$ 2500'
+    },
+    {
+      nome: 'Geladeira',
+      preco: 'R$ 3000'
+    },
+    {
+      nome: 'Celular',
+      preco: 'R$ 1500'
+    },
+    {
+      nome: 'gUITARRA',
+      preco: 'R$ 3500'
+    },
+  ],
+  ativa: false,
+}
 const App = () => {
+ const dados = luana;
 
-function mostrarNome(sobrenome){
-  return 'Bruno ' + sobrenome;
-}
-
-const carro ={
-  marca: 'ford',
-  rodas: '4',
-};
-
-const estiloP = {
-  color: 'blue',
-  fontSize: '1rem'
-  
-}
+const total = dados.compras.map(item => Number(item.preco.replace("R$",""))).reduce((a, b) => a + b);
 
 
-  return (
-<>
-<p>
-  {true ? 'verdadeiro' : 'falso'} - {10}
-  {mostrarNome('Eduardo')}
-  <p style={estiloP}>GREMIOOOO</p>
-</p>
-  <label htmlFor="nome">Nome</label>
-  <input type="text" id="nome"></input>
-</>
-);
+  return <div>
+    <p>Nome: {dados.cliente}</p>
+    <p>Idade: {dados.idade}</p>
+    <p>Situação: <span style={{color: dados.ativa ? 'green' : 'red'}}>{dados.ativa ? 'Ativa' : 'Inativa'}</span></p>
+    <p>Total: R$ {total}</p>
+ {total > 10000 &&  <p>"Você está gastando muito zé"</p>}
+
+
+
+  </div>
+
+ 
 };
 
 export default App;
