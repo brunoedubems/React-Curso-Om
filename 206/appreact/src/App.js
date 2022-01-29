e19d01b8f9437439de8dcc8ad499da405ccc0495
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-01-29 17:08:05 -0300

    Update App.js

--- 206/appreact/src/App.js
+++ 206/appreact/src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Meu app</div>;
+
+function mostrarNome(sobrenome){
+  return 'Bruno ' + sobrenome;
+}
+
+const carro ={
+  marca: 'ford',
+  rodas: '4',
+};
+
+const estiloP = {
+  color: 'blue',
+  fontSize: '1rem'
+  
+}
+
+
+  return (
+<>
+<p>
+  {true ? 'verdadeiro' : 'falso'} - {10}
+  {mostrarNome('Eduardo')}
+  <p style={estiloP}>GREMIOOOO</p>
+</p>
+  <label htmlFor="nome">Nome</label>
+  <input type="text" id="nome"></input>
+</>
+);
 };
 
 export default App;
