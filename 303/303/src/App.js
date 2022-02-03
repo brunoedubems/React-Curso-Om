19874c9489f51a845e350d97d0feaae187bbfb09
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-02-03 17:52:19 -0300

    302 desafio

--- 303/303/src/App.js
+++ 303/303/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { Produto } from './Produto';
 
 
 const App = () => {
-  const [contar, setContar] = React.useState(5);
+  const [produto, setProduto] = React.useState(null);
+
+React.useEffect(() => {
+const produtolocal = window.localStorage.getItem('produto');
+if(produtolocal !== null ) setProduto(produtolocal)
+}, []);
+
+
+React.useEffect(() =>{
+  if(produto !== null) window.localStorage.setItem('produto', produto);
+},[produto])
+
+  
+function handleClick(event) {
+  setProduto(event.target.innerText);
+}
 
 
 
-return <button onClick={() => setContar(contar +5)}>{contar}</button>
+return(
+<div>
+<h1>Preferência: {produto}</h1>
+
+<button style={{marginRight: '1rem'}} onClick={handleClick}>Notebook</button>
+<button style={{marginRight: '1rem'}} onClick={handleClick}>Smartphone</button>
+
+<Produto produto={produto}/>
+
+</div>
+);
+
 };
 
 export default App;
