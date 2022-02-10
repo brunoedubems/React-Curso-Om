be2e3e798c5e4e4935fa28bd00b42ccf79d470f0
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-02-09 21:09:55 -0300

    Update App.js

--- 303 aula/aula303/src/App.js
+++ 303 aula/aula303/src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 
 const App = () => {
+const [comentarios, setComentarios] = React.useState([]);
+const [input, setInput] = React.useState();
+const inputElement = React.useRef();
+
+
+
+function handleClick(){
+  setComentarios([...comentarios, input]);
+  setInput('');
+  inputElement.current.focus();
+}
 
   return <div>
-
-
-
+    <ul>
+      {comentarios.map(
+        comentarios => <li key={comentarios}>{comentarios}</li>
+        )};
+    </ul>
+  <input type="text" 
+  ref={inputElement}
+  value={input} 
+  onChange={({target}) =>setInput(target.value)}
+  />
+<button onClick={handleClick}>Enviar</button>
   </div>;
 }
 export default App;
